e131d66a20de2bdd2a6e5dad30a827b809459610
EXPEDITO TERTO DA SILVA NETO <etsn1@aluno.ifal.edu.br>
2026-09-04 21:27:38 -0300

    Checkpoint antes da responsividade

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const menu = document.querySelector(".menu");
 
 botaoMenu.addEventListener("click", () => {
   menu.classList.toggle("aberto");
+  if (menu.classList.contains("aberto")) {
+    botaoMenu.innerHTML = "";
+  } else {
+    botaoMenu.innerHTML = "";
+  }
 });
