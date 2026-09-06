6b3113795fd071ce729f310e7c4ea63278768433
EXPEDITO TERTO DA SILVA NETO <etsn1@aluno.ifal.edu.br>
2026-09-06 13:17:09 -0300

    Acrescentado carrossel de imagens

--- script.js
+++ script.js
@@ -23,3 +23,42 @@ cards.forEach((card) => {
     card.classList.toggle("focado");
   });
 });
+
+const imagem = document.querySelector(".imgProduto");
+const setaEsquerda = document.querySelector(".esquerda");
+const setaDireita = document.querySelector(".direita");
+
+let imagemAtual = 0;
+
+const imagens = [
+  "imgs/tortilete1.jpg",
+  "imgs/tortilete2.jpg",
+  "imgs/tortilete3.jpeg",
+  "imgs/tortilete4.jpg",
+];
+
+setaEsquerda.addEventListener("click", () => {
+  if (imagemAtual === 0) {
+    trocarImagem(imagens.length - 1);
+  } else {
+    trocarImagem(imagemAtual - 1);
+  }
+});
+
+setaDireita.addEventListener("click", () => {
+  if (imagemAtual === imagens.length - 1) {
+    trocarImagem(0);
+  } else {
+    trocarImagem(imagemAtual + 1);
+  }
+});
+
+function trocarImagem(novoIndice) {
+  imagem.style.opacity = 0;
+  setTimeout(() => {
+    imagemAtual = novoIndice;
+    imagem.src = imagens[imagemAtual];
+    imagem.style.opacity = 1;
+  }, 300);
+  console.log(imagemAtual);
+}
